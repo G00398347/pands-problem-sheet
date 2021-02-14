ebd3150a79a9fac8f349e3dc2c6d4556dd0cb58f
Ruth McQuillan <ruthpmcquillan@gmail.com>
2021-02-14 17:36:36 +0000

    Week 03

---
+++ Week 03/secondstring.py
@@ -0,0 +1,5 @@
+#this programe asks a user to input a string and outputs every second letter in reverse order.
+#Author : Ruth McQuillan
+
+strSentance = input ('Please enter a sentence:')         # this line asks the user to input the sentence
+
