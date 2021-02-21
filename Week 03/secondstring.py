5ca45246067c7d3aad9a93304fbf3538b6d39f05
Ruth McQuillan <ruthpmcquillan@gmail.com>
2021-02-21 21:05:20 +0000

    Week 03

--- Week 03/secondstring.py
+++ Week 03/secondstring.py
@@ -1,5 +1,7 @@
 #this programe asks a user to input a string and outputs every second letter in reverse order.
 #Author: Ruth McQuillan
 
-strSentance = input ('Please enter a sentence:')         # this line asks the user to input the sentence
+strSentence = input ("Please enter a sentence: ")         #this line asks the user to input the sentence
+str2 = (strSentence [::-2])                               #this line prints every second character in the string backwards
+print (str2)                                              #this command prints the required output
 
